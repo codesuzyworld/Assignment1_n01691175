1b722ba03281ceadb01101dc395bab1b6ec968b1
suzanna8767 <31928754+suzanna8767@users.noreply.github.com>
2024-05-27 12:21:33 -0400

    Created Route Controller and Example Controller during class on Week 4.

---
+++ Assignment1_n01691175/Controllers/ExampleController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Assignment1_n01691175.Controllers
+{
+    public class ExampleController : ApiController
+    {
+        //We have the ability to listen for http requests and provide 
+        // responses to those requests
+
+        //Framework: a set of tools that allow us to achieve a given task
+        //Abstraction: obfuscating the details of an implementation to allow
+        //a user to focus on the overall strategy e.g. hide stuff so ppl wont be overwhelmed
+        //Convention: A set of predefined processes or forms which allow us to achieve
+        //our given tasks
+
+        //Get:localhost:xx/api/Example -> "My http response"
+        // go to the command prompt and type 
+        // curl https://localhost:44339/api/example
+        public string Get()
+        {
+            string response = "A GET request";
+            return response;
+        }
+
+        //POST: localhost:xx/api/Example
+        // go to the command prompt and type 
+        // Post request requires you to send some form data to get a response
+        // let's send some empty data to get the post response here. 
+        // curl -d "" https://localhost:44339/api/example   
+        
+        /// <summary>
+        /// This method listens for a post request and provides a string response
+        /// </summary>
+        /// <returns>
+        /// A response to a post request
+        /// </returns>
+        /// <example>
+        /// Post:localhost:xx/Apli/Example -> "A response to a POST request"
+        /// /// curl -d "" https://localhost:44339/api/example
+        /// </example>
+        
+        public string Post()
+        {
+            string response = "A POST REQUEST";
+            return response;
+        }
+
+
+
+        //Get:localhost:xx/api/Example/id number -> "Get with number input"
+     /// <summary>
+     /// Recieves a number and returns a message indicating what number was entered.
+     /// </summary>
+     /// <param name="id"></param>
+     /// <returns>
+     /// "You entered the number {id}"
+     /// </returns>
+    /// <example>
+    /// 
+    /// </example>
+
+        public string GET(int id)
+        {
+            string message = "You entered number ";
+            message = message + id;
+            return message;
+        }
+
+
+    }
+}
---
+++ Assignment1_n01691175/Controllers/RouteController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Routing;
+
+namespace Assignment1_n01691175.Controllers
+{
+    public class RouteController : ApiController
+    {
+        //in webapi config(to adjust the behavior of the default route template)
+        //routeTemplate: "api/{controller}/{action}/{id}",
+
+
+        //Get: api/Route/Method1 -> "method 1"
+        [HttpGet]
+        [Route("api/Route/Method1")]
+        public string Method1()
+        {
+            return "method 1";
+        }
+
+        //Get: api/Route/Method2 -> "method 2"
+        [HttpGet]
+        [Route("api/Route/Method2")]
+        public string Method2()
+        {
+            return "method 2";
+        }
+
+        //Get: api/Route/Method3/{id}
+        [HttpGet]
+        [Route("api/Route/Method3/{id}")]
+        public string Method3(int id)
+        {
+            string message = "Method 3 has an input of ";
+            message = message + id;
+            return message;
+        }
+
+
+        /// <summary>
+        /// Recieves two inputs and outputs their value
+        /// </summary>
+        /// <param name="input1"> the first input </param>
+        /// <param name="input2"> the second input </param>
+        /// <returns>
+        /// <example>
+            ///Get: api/Route/Method4/{input1}/{input2}
+            ///input 1 is {input 1} , input 2 is {input 2}
+        /// </example>
+        /// </returns>
+
+        [HttpGet]
+        [Route("api/Route/Method4/{input1}/{input2}")]
+        public string Method4(int input1, int input2)
+        {
+            string output = "input 1 is " + input1;
+            output = output + " and input 2 is " + input2;
+            return output;
+        }
+
+
+        //Post: api/Route/Method5
+        //>curl -d "this is post data" https://localhost:44339//api/Route/Method5
+        //"our Post request"
+        [HttpPost]
+        [Route("api/Route/Method5")]
+        public string Method5()
+        {
+            return "our Post request";
+        }
+    }
+}
